5d47c122d94a64660902b4c4323840b708cb6b47
Ankur Dev <a.dev@se16.qmul.ac.uk>
2018-10-27 14:48:10 +0100

    comments added

--- W2/pi.f90
+++ W2/pi.f90
@@ -2,7 +2,7 @@
        implicit double precision(i)
        implicit double precision(s)
        implicit double precision(o)
-
+       !This calculates the value of pi
        sum = 0
        DO i = 1,10000000
           s = s + (1/(i*i))
