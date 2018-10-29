62e63932cc0fc756a2431de8a32114d82597f25a
Ankur Dev <ankurdev93@gmail.com>
2018-10-29 18:39:48 +0000

    Week 5 Completed

--- W5/silica/read.f
+++ W5/silica/read.f
@@ -1,60 +1,49 @@
         program read
 10      format (A70)
 20      format (I10,1p,E14.6,0p,I10)
-30      format (1p,5E14.6)
         implicit double precision (a-h,o-z)
         dimension c(3,3)
-        !parameter (N_max=1000000)
-        !dimension a(N_max,3),v(3,3)
         
         open(10,file='HISTORY',status='old')
         open(20,file='velocity')
-        write(20,*) 'timestep ' , 'avsi ', 'avo'
-
+        write(20,*) 'Timestep   ' , 'avsi   ', 'avo   ', 'Temperature'
+        
+        !Ignoring first two lines
         read(10,10) xnoneed
         read(10,10) xnoneed
         
-        DO m = 1, 60
+        DO m = 1, 60  !Outer loop on number of timesteps
         read(10,10) xnoneed ! skips the timestep line
 
         vsi = 0
         vo = 0
         DO i=1,3
                 read(10,*)(c(i,j),j=1,3) !stores value in c(3,3) array
-                !print *,v(i,j)
         ENDDO
-        !print *,c(1,1),c(2,1),c(3,1) !prints elements of c
        
-        DO k = 1,648
+        DO k = 1,648  !loop over number of atoms
                 read(10,10) xnoneed !Skips the info of Si atom line
                 read(10,10) xnoneed !skips the coordinate line
                 read(10,*) vx,vy,vz !Reads velocities
-                !print *, vx,vy,vz
                 
                 if(k.le.216) then
                         vsi = vsi + vx**2 + vy**2 + vz**2
-                        !print *, vsi
                 else
                         vo = vo + vx**2 + vy**2 + vz**2
-                        !print *,'Vo',vo
                 endif       
-        ENDDO
-        amu = 1.6605E-27
-        zK_Si = (vsi*1000*28.09*amu)/2
-        zK_O = (vo*1000*16.0*amu)/2
-        zKsys = K_Si + K_O
-        zN = 648
-        zKb = 0.1381E-22
-        temp = (2*zKsys)/(3*zN*zKb)
-        print *, temp
+        ENDDO  !Ends loop on number of atoms
+        amu = 1.6605E-27 !Define a.m.u in Kg
+        zK_Si = (vsi*(10**4)*28.09*amu)/2 !Energy for Silicon atoms
+        zK_O = (vo*(10**4)*16.0*amu)/2  !Energy for Oxygen atoms
+        zKsys = zK_Si + zK_O  !Total System Energy
+        zN = 648  !Total atoms in System
+        zKb = 0.1381E-22  !Define Boltzman Constant
+        temp = (2*zKsys)/(3*zN*zKb)  !Calculate temperate
 
         avsi = vsi/216
         avo = vo/432
         timestep = (m-1)*50
-        write(20,*) timestep , avsi, avo
-        !read(10,20)n1,t,nval
-        !read(10,30)(a(k),k=1,nval)
-        !write(20,*)t,a(1)
-        !read(10,*)(v(i,j),j=1,3)
-        ENDDO
+        write(20,*) timestep , avsi, avo, temp
+        
+        ENDDO   !Ends the outer loop on number of timesteps
         end program read
