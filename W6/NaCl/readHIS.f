33cbe6a1ed10a30471fe741e6d08c9fca3662d06
Ankur Dev <ankurdev93@gmail.com>
2018-11-02 10:23:10 +0000

    Week6 completed

--- W6/NaCl/readHIS.f
+++ W6/NaCl/readHIS.f
@@ -5,14 +5,14 @@
 40      format (A70)
         implicit double precision (a-h,o-z)
                 
-        open(10,file='HIS50',status='old')
-        open(20,file='position')
-        open(30,file='HIS500',status='old')
-        open(40,file='HIS5000',status='old')
+        open(10,file='HIS50',status='old') !Open HIS50 file
+        open(20,file='position')  !Open file to store data
+        open(30,file='HIS500',status='old') !Open HIS500 file
+        open(40,file='HIS5000',status='old') !Open HIS5000 file
 
         write(20,*) 'Timestep  ' , 'Px(50K)  ', 'Px(500K)  ', 'Px(500)'
         
-        !Ignoring first two lines
+        !Ignoring first two lines of each file
         read(10,10) xnoneed
         read(10,10) xnoneed
         read(30,30) xnoneed
@@ -22,10 +22,13 @@
         
         DO m = 1, 80  !Outer loop on number of timesteps
         timestep = (m-1)*50
-        read(10,10) xnoneed ! skips the timestep line
+        
+        ! skips the timestep line
+        read(10,10) xnoneed
         read(30,30) xnoneed
         read(40,40) xnoneed
 
+        !ignore box size info
         DO i=1,3
                 read(10,10) xnoneed
                 read(30,30) xnoneed
@@ -33,17 +36,17 @@
         ENDDO
        
         DO k = 1, 512  !loop over number of atoms
-		
+                
                 if(k.le.1) then
                          read(10,10) xnoneed !Skips the info of Na atom line
-                         read(10,*) px_50 !Reads velocities
+                         read(10,*) px_50 !Reads position Px from HIS50
                          read(30,30) xnoneed !Skips the info of Na atom line
-			 read(30,*) px_500
-			 read(40,40) xnoneed !Skips the info of Na atom line
+                         read(30,*) px_500
+                         read(40,40) xnoneed !Skips the info of Na atom line
                          read(40,*) px_5000
                          !print *, px_50, px_500, px_5000
-			 write(20,*) timestep, px_50, px_500, px_5000
-                else
+                         write(20,*) timestep, px_50, px_500, px_5000
+                else !ignore rest of it
                          read(10, 10) xnoneed
                          read(10, 10) xnoneed
                          read(30, 30) xnoneed
@@ -53,7 +56,5 @@
                 endif
         ENDDO
 
-        !write(20,*) timestep , , avo, temp
-        
         ENDDO   !Ends the outer loop on number of timesteps
         end program read
