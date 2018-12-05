3ebdde104cda5a218b333b7228232dc1bf373d13
Ankur Dev <a.dev@se16.qmul.ac.uk>
2018-12-05 17:28:34 +0000

    Prac5 movie not done

---
+++ W8/discard/convert_atomeye.f95
@@ -0,0 +1,116 @@
+        implicit real*8 (a-h,o-z)
+        Parameter (N_max=3300000)
+        dimension a(N_max,3),b(N_max,3),v(3,3)
+        character*8 names(N_max)
+        nframes=10
+        natoms=512
+        atm_o=16.0
+        atm_si=28.09
+        atm_zr=91.22
+        atm_u=238.04
+        open(10,file='HIS50',status='old')
+        
+        
+  10    Format(A70)
+  20    Format(A21,1x,I7)
+  30    Format(A8,6x,I4,42x,F10.7) 
+  40    Format(A2)
+  50    Format(A3,1x,F5.1,1x,A8)
+  60    Format(A9,1x,F7.2,1x,A1)
+  70    Format(F6.2,1x,A2,6F9.4)
+        read(10,10) xnoneed
+        read(10,10) noneed
+        
+        DO i=1,nframes
+         
+          
+          read(10,30) xnoneed,nstep,time
+          
+          Do ii=1,3
+          read(10,*)(v(ii,k),k=1,3)
+          enddo
+          d1=v(1,1)
+          d2=v(2,2)
+          d3=v(3,3)
+          x0=0.1812068722
+          y0=0.7243275536
+          z0=3.721142589
+
+          DO j=1,natoms
+           read(10,40) names(j)
+           read(10,*) a(j,1),a(j,2),a(j,3)
+          ! read(10,*)
+          enddo
+          call open_write_sav(20,'s',i)
+       write(20,20) 'Number of particles =',natoms
+       write(20,50) 'A =',1.0,'Angstrom'
+       write(20,60) 'H0(1,1) =',v(1,1),'A'
+       write(20,60) 'H0(1,2) =',v(1,2),'A'
+       write(20,60) 'H0(1,3) =',v(1,3),'A'
+       write(20,60) 'H0(2,1) =',v(2,1),'A'
+       write(20,60) 'H0(2,2) =',v(2,2),'A'
+       write(20,60) 'H0(2,3) =',v(2,3),'A'
+       write(20,60) 'H0(3,1) =',v(3,1),'A'
+       write(20,60) 'H0(3,2) =',v(3,2),'A'
+       write(20,60) 'H0(3,3) =',v(3,3),'A'
+           do j=1,natoms
+            if(names(j).eq.'Si') then
+             write(20,70)atm_si,'Si',a(j,1)/d1,a(j,2)/d2,a(j,3)/d3,0.d0,0.d0,0.d0
+            else if(names(j).eq.'O')then
+             write(20,70)atm_o,'O',a(j,1)/d1,a(j,2)/d2,a(j,3)/d3,0.d0,0.d0,0.d0
+            else if(names(j).eq.'Zr')then
+             write(20,70) atm_zr,'Zr',a(j,1)/d1,a(j,2)/d2,a(j,3)/d3,0.d0,0.d0,0.d0
+            else if(names(j).eq.'U')then
+            write(20,70) atm_u,'U',a(j,1)/d1,a(j,2)/d2,a(j,3)/d3,0.d0,0.d0,0.d0
+
+        endif
+        enddo
+
+      
+         
+         close(20)
+        enddo
+          
+          end
+
+      subroutine open_write_sav(unit,file,ncfgsaved)
+      integer unit,ncfgsaved
+      character*(*) file,extension*10,name*32
+      if (ncfgsaved.lt.10)then
+      write(extension,1101) ncfgsaved
+      else if (ncfgsaved.lt.100) then
+      write(extension,1102) ncfgsaved
+      else if (ncfgsaved.lt.1000) then
+      write(extension,1103) ncfgsaved
+      else if (ncfgsaved.lt.10000) then
+      write(extension,1104) ncfgsaved
+      endif
+ 1101  format('_',i1,'.cfg',3x)
+ 1102  format('_',i2,'.cfg',3x)
+ 1103  format('_',i3,'.cfg',2x)
+ 1104  format('_',i4,'.cfg',1x)
+      I=1
+      J=LEN(FILE)
+      DO K=1,J
+        IF(FILE(K:K).EQ.' ')THEN
+          I=I+1
+        ELSE
+          GOTO 3000
+        ENDIF
+      ENDDO
+ 3000 CONTINUE
+      DO K=J,1,-1
+        IF(FILE(K:K).EQ.' ')THEN
+          J=J-1
+        ELSE
+          GOTO 3010
+        ENDIF
+      ENDDO
+ 3010 CONTINUE
+      name=file(I:J)//extension
+      open (unit,file=name,STATUS='UNKNOWN')
+      end
+
+
+
+
---
+++ W8/movie5000/convert_NACl_atomeye.f
@@ -0,0 +1,115 @@
+        implicit real*8 (a-h,o-z)
+        Parameter (N_max=800000)
+        dimension a(N_max,3),b(N_max,3),v(3,3)
+        nframes= 60
+        natoms=512
+        nuatoms=1
+        nmgatoms=863
+        nclatoms = 256
+        nnaatoms = 256
+        noatoms=864
+        atm_o=16.0
+        atm_u=238.04
+        atm_na = 23.0
+        atm_cl = 35.0
+        atm_mg=24.31
+        bk=1.38e-23
+        open(10,file='HIS5000',status='old')
+        
+        
+  10    Format(A70)
+  20    Format(A21,1x,I7)
+  30    Format(A8,6x,I4,42x,F10.7) 
+  40    Format(A2)
+  50    Format(A3,1x,F5.1,1x,A8)
+  60    Format(A9,1x,F7.2,1x,A1)
+  70    Format(F6.2,1x,A2,6F9.4)
+        read(10,10) xnoneed
+        read(10,10) noneed
+        
+        DO i=1,nframes
+         
+          
+          read(10,30) xnoneed,nstep,time
+          
+          Do l=1,3
+          read(10,*)(v(l,k),k=1,3)
+          enddo
+          d1=v(1,1)
+          d2=v(2,2)
+          d3=v(3,3)
+          DO j=1,natoms
+           read(10,40) atype
+           read(10,*) a(j,1),a(j,2),a(j,3)
+          
+          enddo
+          call open_write_sav(20,'s',i)
+       write(20,20) 'Number of particles =',natoms
+       write(20,50) 'A =',1.0,'Angstrom'
+       write(20,60) 'H0(1,1) =',v(1,1),'A'
+       write(20,60) 'H0(1,2) =',v(1,2),'A'
+       write(20,60) 'H0(1,3) =',v(1,3),'A'
+       write(20,60) 'H0(2,1) =',v(2,1),'A'
+       write(20,60) 'H0(2,2) =',v(2,2),'A'
+       write(20,60) 'H0(2,3) =',v(2,3),'A'
+       write(20,60) 'H0(3,1) =',v(3,1),'A'
+       write(20,60) 'H0(3,2) =',v(3,2),'A'
+       write(20,60) 'H0(3,3) =',v(3,3),'A'
+        DO j=1,natoms
+       if(j.le.256) then
+        write(20,70) atm_na,'Na',a(j,1)/d1,a(j,2)/d2,a(j,3)/d3, 
+     &  0.d0,0.d0,0.d0
+       Else
+            write(20,70) atm_cl,'Cl',a(j,1)/d1,a(j,2)/d2,a(j,3)/d3,
+     &  0.d0,0.d0,0.d0
+
+
+         endif
+           enddo
+         close(20)
+        enddo
+          
+          end
+
+      subroutine open_write_sav(unit,file,ncfgsaved)
+      integer unit,ncfgsaved
+      character*(*) file,extension*10,name*32
+      if (ncfgsaved.lt.10)then
+      write(extension,1101) ncfgsaved
+      else if (ncfgsaved.lt.100) then
+      write(extension,1102) ncfgsaved
+      else if (ncfgsaved.lt.1000) then
+      write(extension,1103) ncfgsaved
+      else if (ncfgsaved.lt.10000) then
+      write(extension,1104) ncfgsaved
+      endif
+ 1101  format('_',i1,'.cfg',3x)
+ 1102  format('_',i2,'.cfg',3x)
+ 1103  format('_',i3,'.cfg',2x)
+ 1104  format('_',i4,'.cfg',1x)
+C We find the relevant part of file.
+      I=1
+      J=LEN(FILE)
+      DO K=1,J
+        IF(FILE(K:K).EQ.' ')THEN
+          I=I+1
+        ELSE
+          GOTO 3000
+        ENDIF
+      ENDDO
+ 3000 CONTINUE
+      DO K=J,1,-1
+        IF(FILE(K:K).EQ.' ')THEN
+          J=J-1
+        ELSE
+          GOTO 3010
+        ENDIF
+      ENDDO
+ 3010 CONTINUE
+      name=file(I:J)//extension
+      open (unit,file=name,STATUS='UNKNOWN')
+      end
+
+
+
+
